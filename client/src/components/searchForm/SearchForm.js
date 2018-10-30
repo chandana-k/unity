aba8dfb11e92d5556acfd009c70a417a9ff558d2
Burnsy <btburns10@gmail.com>
2018-10-30 19:45:29 -0400

    merge request

--- client/src/components/searchForm/SearchForm.js
+++ client/src/components/searchForm/SearchForm.js
@@ -1,49 +1,3 @@
-<<<<<<< Updated upstream
-import React, { Component } from 'react';
-import './searchForm.css';
-import StockItem from '../stockItem';
-import API from '../../utils/API';
-
-export default class SearchFrom extends Component {
-  state = {
-    search: '',
-    stock: []
-  }
-  componentDidMount() {
-    API.companySearch('AAPL')
-    .then(res => console.log(res.data.quote))
-    .catch(err => console.log(err))
-  }
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-  handleSubmit = event => {
-    event.preventDefault();
-    API.companySearch(this.state.search)
-    .then(res => this.setState({ stock: res.data.quote }))
-    .catch(err => console.log(err))
-  };
-  render() {
-    return (
-      <div className="searchForm">
-        search me
-        <form onSubmit={this.handleSubmit}>
-          <input
-            name="search"
-            type="text"
-            value={this.state.search}
-            onChange={this.handleChange('search')} 
-          />
-          <button type="submit">Search</button>
-        </form>
-        <StockItem stock={this.state.stock} />
-      </div>
-    );
-  }
-}
-=======
 // import React, { Component } from "react";
 // import "./searchForm.css";
 // import API from '../../utils/API';
@@ -80,4 +34,3 @@ export default class SearchFrom extends Component {
 //     );
 //   }
 // }
->>>>>>> Stashed changes
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,21 +4,6 @@ import { csv } from "d3-request";
 import { timeParse } from "d3-time-format";
 
 export default {
-<<<<<<< Updated upstream
-  stockSearch (ticker) {
-    const API_KEY = 'NSUNV8LPVSSN0247'
-    const url = 'https://www.alphavantage.co/query?' +
-            'function=TIME_SERIES_INTRADAY' + 
-            '&symbol=' + ticker +
-            '&apikey=' + API_KEY +
-            '&datatype=csv';
-    return axios.get(url);
-  },
-  companySearch (ticker) {
-    const url = 'https://api.iextrading.com/1.0/stock/' + ticker + '/batch?types=quote,news&range=1m&last=5';
-    return axios.get(url);
-  }
-=======
   auth: (user) => {
     console.log(user)
     return axios.post('/login', user)
@@ -36,7 +21,6 @@ export default {
   //           '&outputsize=full'
   //   return axios.get(url)
   // }
->>>>>>> Stashed changes
 };
 
 function parseData(parse) {
